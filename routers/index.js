93cecd6984ed6103855f475cd2e842e7364abc1c
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-05-29 01:06:56 +0900

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '욕은 금물입니다..' });
+  res.render('index', { title: '자동업데이트 확인!' });
 });
 
 module.exports = router;
