3b8f4fad540bc6a9381f3a1c2510a2c07be92a60
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-05-29 00:18:26 +0900

    욕은 금물입니다..

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '이런 씨바 세이~!!' });
+  res.render('index', { title: '욕은 금물입니다..' });
 });
 
 module.exports = router;
