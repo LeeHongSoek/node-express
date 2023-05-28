67b5650c2cc1d1a872f32a7eaa2131d56a15796a
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-05-29 01:29:51 +0900

    파일명변경

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const ws = require('ws');
 
-var indexRouter = require('./routers/index');
-var usersRouter = require('./routers/users');
+// Create a new Express app.
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Create a new WebSocket server.
+const wss = new ws.Server({
+  server: app.listen(3000)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Handle WebSocket connections.
+wss.on('connection', (ws) => {
+  // Send a message to the client.
+  ws.send('Hello, client!');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // Listen for messages from the client.
+  ws.on('message', (message) => {
+    console.log('Received message from client:', message);
+  });
 });
 
-module.exports = app;
+// Start the server.
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
