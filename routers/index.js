e89822365cbc92c04981de4f38b326ab8c726a92
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2023-05-29 01:51:12 +0900

    console.log('./routers/.. 실행'); 추가

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('./routers/index');
+  console.log('./routers/index 실행');
   res.render('index', { title: '자동업데이트 확인!' });
 });
 
--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('./routers/users 실행');
   res.send('respond with a resource');
 });
 
