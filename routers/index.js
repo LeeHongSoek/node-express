947a2183481619933efefc1c44424c660d644be7
이홍석(GMail) <lhs0806@gmail.com>
2023-05-30 16:29:48 +0900

    '타이틀 ./routers/index '

--- routers/index.js
+++ routers/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('라우팅 : ./routers/index');
-  res.render('index', { title: '자동업데이트 확인!' });
+  res.render('index', { title: '타이틀 ./routers/index ' });
 });
 
 module.exports = router;
